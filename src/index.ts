2e876b13eb137c6de427bf01468306c49aa3b8b7
longtruong2604 <longtruong2604@gmail.com>
2025-03-06 23:17:10 +0700

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,30 @@
-import cors from 'cors'
 import dotenv from 'dotenv'
 import express, { Application, Request, Response } from 'express'
+import { authMiddleware } from './middlewares/auth.middleware'
+import corsMiddleware from './middlewares/cors.middleware'
 import { invalidPathHandler } from './middlewares/error.middleware'
+import loggerMidleware from './middlewares/logger.middleware'
 import { analysisRoutes } from './routes/cttAnalysis.routes'
 import { uploadRoutes } from './routes/upload.routes'
-import logger from './utils/logger'
-import morgan from 'morgan'
-import { authMiddleware } from './middlewares/auth.middleware'
+import swaggerUi from 'swagger-ui-express'
+import { swaggerDocs } from './utils/swaggerConfig'
 
 dotenv.config()
 
 const app: Application = express()
 const port = process.env.PORT || 3000
-
-const allowedOrigins = ['https://lifeistoolong.id.vn', 'https://www.lifeistoolong.id.vn']
-
-const morganFormat = ':method :url :status :response-time ms'
-
-app.use(
-  morgan(morganFormat, {
-    stream: {
-      write: (message) => {
-        const logObject = {
-          method: message.split(' ')[0],
-          url: message.split(' ')[1],
-          status: message.split(' ')[2],
-          responseTime: message.split(' ')[3],
-        }
-        logger.info(JSON.stringify(logObject))
-      },
-    },
-  })
-)
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-    methods: 'GET,POST,PUT,DELETE',
-    credentials: false,
-  })
-)
+app.use(express.json())
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+app.use(loggerMidleware)
+app.use(corsMiddleware)
 
 app.use('/api', authMiddleware, uploadRoutes)
 app.use('/api', analysisRoutes)
-
-app.use(invalidPathHandler)
-
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server')
 })
+
+app.use(invalidPathHandler)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`)
